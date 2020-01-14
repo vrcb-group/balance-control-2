cdb95924231308bf9717147a86dbfb19781494f7
sgopala6 <sgopala6@ncsu.edu>
2020-01-14 12:02:12 +0100

    abstract scene added to project

---
+++ Assets/SpherePoolScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpherePoolScript : MonoBehaviour
+{
+    public GameObject SpherePool;
+    public int amount = 100;
+    List<GameObject> sphere;
+
+    void Start()
+    {
+        sphere = new List<GameObject>();
+
+        for(int i=0; i<amount; i++)
+        {
+            GameObject sph = (GameObject)Instantiate(SpherePool);
+            sph.SetActive(false);
+            sphere.Add(sph);
+        }
+
+    }
+
+    
+}
---
+++ Assets/Spheres.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spheres : MonoBehaviour
+{
+    public GameObject Sphere;
+
+    void Start()
+    {
+        //for (int x= -10; x<10; x++)
+        //{
+        //    for (int y = -10; y < 10; y++)
+        //    {
+        //        for (int z = -10; z < 10; z++)
+        //        {
+        //            Instantiate(Sphere, new Vector3(x, y, z), Quaternion.identity);
+        //        }
+        //    }
+        //}
+
+        for (int i = 0; i < 2000; i++)
+        {
+            Instantiate(Sphere, new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-11.0f, 15.0f), Random.Range(-10.0f, 10.0f)), Quaternion.identity);
+        } 
+    }
+
+    void Update()
+    {
+        
+    }
+}
